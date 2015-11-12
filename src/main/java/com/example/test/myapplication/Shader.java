31661fba57cd19cec4df4c40c6c87a90d226fd6b
weg896 <weg896@mail.usask.ca>
2015-11-12 15:06:58 -0600

    pratice code for openGL ES tutorial 1 --2

--- src/main/java/com/example/test/myapplication/Shader.java
+++ src/main/java/com/example/test/myapplication/Shader.java
@@ -1,7 +1,31 @@
 package com.example.test.myapplication;
 
+import android.opengl.GLES30;
+
 /**
  * Created by test on 11/12/2015.
  */
 public class Shader {
+
+    private final static String vertcode =
+            "attribute vec4 a_pos;"+
+                "void main(){"+
+                "gl_Position = a_pos;"+
+                "}";
+    private final static String fragcode =
+            "precision mediump float;"+
+                    "uniform vec4 u_color;"+
+                    "void main(){"+
+                    "gl_FragColor = u_color;"+
+                    "}";
+
+    private static int program;
+
+    public static int positionhandle;
+    public static int colorhandle;
+
+    public static void makeprogram(){
+        int vertexshader = loadshader(GLES30.GL_VERTEX_SHADER, vertcode)
+    }
+
 }
